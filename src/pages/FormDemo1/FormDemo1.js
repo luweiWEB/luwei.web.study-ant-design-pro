530d84492201d2ddaaa661a97aebc9afe42c4544
luweiWEB <web@talkmoney.cn>
2018-11-29 21:22:19 +0800

    <D> 删除 删除所有组件页面

--- src/pages/FormDemo1/FormDemo1.js
+++ src/pages/FormDemo1/FormDemo1.js
@@ -1,74 +1,9 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'dva';
-import {
-  Form,
-  Input,
-  DatePicker,
-  Select,
-  Button,
-  Card,
-  InputNumber,
-  Radio,
-  Icon,
-  Tooltip,
-  Row,
-  Col,
-} from 'antd';
+import styles from './FormDemo1.css';
 
-@Form.create()
-class FormDemo1 extends PureComponent {
-  handleSubmit = event => {
-    event.preventDefault();
-
-    const { form } = this.props;
-
-    form.validateFields(['searchName'], (err, fieldsValue) => {
-      if (err) return;
-
-      const { searchName = '' } = fieldsValue;
-
-      alert('查询条件为：' + searchName);
-
-      this.setState({
-        searchName,
-      });
-    });
-  };
-
-  render() {
-    const {
-      form: { getFieldDecorator, getFieldValue },
-    } = this.props;
-
-    const formItemLayout = {
-      labelCol: {
-        xs: { span: 5 },
-        sm: { span: 7 },
-      },
-      wrapperCol: {
-        xs: { span: 5 },
-        sm: { span: 5 },
-        md: { span: 5 },
-      },
-    };
-
-    return (
-      <Form onSubmit={this.handleSubmit} hideRequiredMark style={{ marginTop: 8 }}>
-        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
-          <Col md={13} sm={24}>
-            <Form.Item {...formItemLayout} label={'名称'}>
-              {getFieldDecorator('searchName')(<Input placeholder={'请输入名称'} />)}
-            </Form.Item>
-          </Col>
-          <Col md={8} sm={24}>
-            <Button type="primary" htmlType="submit">
-              查询
-            </Button>
-          </Col>
-        </Row>
-      </Form>
-    );
-  }
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <h1>Page FormDemo1</h1>
+    </div>
+  );
 }
-
-export default FormDemo1;
