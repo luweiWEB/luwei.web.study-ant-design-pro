e68ba3a21cdd97472dee4efd5c1b6d0e734da7f0
luweiWEB <web@talkmoney.cn>
2018-11-30 09:56:43 +0800

    <A> 增加 增加查询业务的组件 —— Form 表单和 Table 组件

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -48,19 +48,7 @@ class UserLayout extends React.PureComponent {
     return (
       // @TODO <DocumentTitle title={this.getPageTitle()}>
       <div className={styles.container}>
-        <div className={styles.lang}>
-          <SelectLang />
-        </div>
         <div className={styles.content}>
-          <div className={styles.top}>
-            <div className={styles.header}>
-              <Link to="/">
-                <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
-              </Link>
-            </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
-          </div>
           {children}
         </div>
         <GlobalFooter links={links} copyright={copyright} />
--- src/pages/FormDemo1/FormDemo1.js
+++ src/pages/FormDemo1/FormDemo1.js
@@ -1,9 +1,99 @@
-import styles from './FormDemo1.css';
+import React, { PureComponent } from 'react';
+import { connect } from 'dva';
+import {
+  Form,
+  Input,
+  Button,
+  Table,
+  Row,
+  Col,
+  Card,
+} from 'antd';
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <h1>Page FormDemo1</h1>
-    </div>
-  );
+@Form.create()
+class FormDemo1 extends PureComponent {
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const { form } = this.props;
+
+    form.validateFields(['searchName'], (err, fieldsValue) => {
+      if (err) return;
+
+      const { searchName = '' } = fieldsValue;
+
+      alert('查询条件为：' + searchName);
+
+      this.setState({
+        searchName,
+      });
+    });
+  };
+
+  render() {
+    const {
+      form: { getFieldDecorator, getFieldValue },
+    } = this.props;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 3 },
+        sm: { span: 5 },
+      },
+      wrapperCol: {
+        xs: { span: 10 },
+        sm: { span: 10 },
+        md: { span: 10 },
+      },
+    };
+    const dataSource = [{
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号'
+    }, {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号'
+    }];
+    
+    const columns = [{
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    }, {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+    }, {
+      title: '住址',
+      dataIndex: 'address',
+      key: 'address',
+    }];
+    
+
+    return (
+        <Card>
+          <Form onSubmit={this.handleSubmit} hideRequiredMark style={{ marginTop: 8 }}>
+            <Row gutter={{ md: 8, lg: 12, xl: 20 }}>
+              <Col md={8} sm={10}>
+                <Form.Item {...formItemLayout} label={'名称'}>
+                  {getFieldDecorator('searchName')(<Input placeholder={'请输入名称'} />)}
+                </Form.Item>
+              </Col>
+              <Col md={12} sm={12}>
+                <Button type="primary" htmlType="submit">
+                  查询
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+    
+          <Table dataSource={dataSource} columns={columns} />
+        </Card>
+    );
+  }
 }
+
+export default FormDemo1;
